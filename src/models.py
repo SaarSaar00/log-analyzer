e29579a5872f255d0fdb0275774f996af87483fe
SaarSaar00 <saranamjoo21@gmail.com>
2026-07-10 18:25:54 +0330

    Initial commit: Add project structure, README, and data models

--- src/models.py
+++ src/models.py
@@ -1,6 +1,5 @@
 class LogEntry:
-
-    def __init__(self, ip, endpoint, status_code, hour):
+    def __init__(self, ip: str, endpoint: str, status_code: int, hour: int):
         self.ip = ip
         self.endpoint = endpoint
         self.status_code = status_code
