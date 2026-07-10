0ada29462b97f65a294b1bdd3bd297929b6563a2
SaarSaar00 <saranamjoo21@gmail.com>
2026-07-10 10:02:23 +0330

    Create LogEntry model

---
+++ src/models.py
@@ -0,0 +1,7 @@
+class LogEntry:
+
+    def __init__(self, ip, endpoint, status_code, hour):
+        self.ip = ip
+        self.endpoint = endpoint
+        self.status_code = status_code
+        self.hour = hour
